895e7646bb633f352c5fa73b001c33abb2cbba67
mattgd <matt.dzwonczyk@live.com>
2016-05-24 13:44:50 -0400

    Renamed DragCircle class to create a Canvas.

--- src/me/mattgd/saywhat/Panel.java
+++ src/me/mattgd/saywhat/Panel.java
@@ -3,33 +3,20 @@ package me.mattgd.saywhat;
 import java.awt.Font;
 import java.awt.FontMetrics;
 import java.awt.Graphics;
-import java.awt.Graphics2D;
-import java.awt.Image;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
 
-import javax.imageio.ImageIO;
 import javax.swing.JPanel;
 
-public class Panel extends JPanel {
+public class Panel extends JPanel
+{
 
 	private static final long serialVersionUID = 1L;
 	
-	private BufferedImage[] balls = new BufferedImage[5]; 
-	private String[] colors = { "red", "green", "blue", "yellow", "magenta" };
-	
-	public Panel() {
-		try {                
-    	   for (int i = 0; i < balls.length; i++) {
-    		   balls[i] = toBufferedImage(ImageIO.read(new File("res/" + colors[i] + ".png")).getScaledInstance(64, 64, 0));
-   			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
+	public Panel()
+	{
     }
 	
-	public void paintComponent(Graphics g) {
+	public void paintComponent(Graphics g)
+	{
 		super.paintComponent(g);
 		
 	    Font f = new Font("SansSerif", Font.BOLD, 14);
@@ -42,28 +29,6 @@ public class Panel extends JPanel {
 	    g.drawString("Say What!", cx, cy);
 	    
 	    cx += fm.stringWidth("Say What!");
-	    
-	    int i = 10;
-	    for (BufferedImage ball : balls) {
-	    	g.drawImage(ball, i, 50, null);
-	    	i += 100;
-	    }
-	}
-	
-	public BufferedImage toBufferedImage(Image img) {
-	    if (img instanceof BufferedImage)
-	    	return (BufferedImage) img;
-
-	    // Create a buffered image with transparency
-	    BufferedImage bimage = new BufferedImage(img.getWidth(null), img.getHeight(null), BufferedImage.TYPE_INT_ARGB);
-
-	    // Draw the image on to the buffered image
-	    Graphics2D bGr = bimage.createGraphics();
-	    bGr.drawImage(img, 0, 0, null);
-	    bGr.dispose();
-
-	    // Return the buffered image
-	    return bimage;
 	}
 
 }
---
+++ src/me/mattgd/saywhat/SayWhatCanvas.java
@@ -0,0 +1,118 @@
+package me.mattgd.saywhat;
+
+import java.awt.BasicStroke;
+import java.awt.Canvas;
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.Rectangle;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseMotionListener;
+
+public class SayWhatCanvas extends Canvas implements MouseListener, MouseMotionListener
+{
+	private static final long serialVersionUID = 1L;
+	
+	private Rectangle rect = new Rectangle(0, 0, 100, 50);
+	private int preX, preY;
+	private boolean isFirstTime = true;
+	private Rectangle area;
+	private boolean pressOut = false;
+
+	public SayWhatCanvas() {
+		setBackground(Color.white);
+	    addMouseMotionListener(this);
+	    addMouseListener(this);
+	}
+
+	public void mousePressed(MouseEvent e) {
+		preX = rect.x - e.getX();
+	    preY = rect.y - e.getY();
+
+	    if (rect.contains(e.getX(), e.getY()))
+	    {
+	    	updateLocation(e);
+	    }
+	    else
+	    {
+	      pressOut = true;
+	    }
+	}
+
+	public void mouseDragged(MouseEvent e)
+	{
+		if (!pressOut) updateLocation(e);
+	}
+
+	public void mouseReleased(MouseEvent e) {
+		if (rect.contains(e.getX(), e.getY()))
+		{
+			updateLocation(e);
+		}
+	    else
+	    {
+	    	pressOut = false;
+	    }
+	}
+
+	public void mouseMoved(MouseEvent e) {}
+	public void mouseClicked(MouseEvent e) {}
+	public void mouseExited(MouseEvent e) {}
+	public void mouseEntered(MouseEvent e) {}
+
+	public void updateLocation(MouseEvent e)
+	{
+		rect.setLocation(preX + e.getX(), preY + e.getY());
+		checkRect();
+	    repaint();
+	}
+
+	public void paint(Graphics g)
+	{
+		update(g);
+	}
+
+	public void update(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+	    Dimension dim = getSize();
+	    int w = (int) dim.getWidth();
+	    int h = (int) dim.getHeight();
+	    g2.setStroke(new BasicStroke(8.0f));
+
+	    if (isFirstTime)
+	    {
+	      area = new Rectangle(dim);
+	      rect.setLocation(w / 2 - 50, h / 2 - 25);
+	      isFirstTime = false;
+	    }
+
+	    // Clears the rectangle that was previously drawn.
+	    g2.setPaint(Color.white);
+	    g2.fillRect(0, 0, w, h);
+
+	    g2.setColor(Color.red);
+	    g2.draw(rect);
+	    g2.setColor(Color.black);
+	    g2.fill(rect);
+	}
+
+	boolean checkRect()
+	{
+		if (area == null) return false;
+		if (area.contains(rect.x, rect.y, 100, 50)) return true;
+		
+	    int newX = rect.x;
+	    int newY = rect.y;
+
+	    if ((rect.x + 100) > area.getWidth()) newX = (int) area.getWidth() - 99;
+	    if (rect.x < 0) newX = -1;
+	    if ((rect.y + 50) > area.getHeight()) newY = (int) area.getHeight() - 49;
+	    if (rect.y < 0) newY = -1;
+	    
+	    rect.setLocation(newX, newY);
+	    return false;
+	}
+}
