8d2056e28ddfdb2e0f8d586766ec8ff275aa96b6
STEPHEN GREENE <SGREENE@hcrhs.k12.nj.us>
2016-06-02 11:41:36 -0400

    fixed arrays

--- src/me/mattgd/saywhat/GameRunner.java
+++ src/me/mattgd/saywhat/GameRunner.java
@@ -22,27 +22,35 @@ public class GameRunner {
 	
 	public static void main(String[] args) {
 		String inputFile = "phrases.txt";
-		ArrayList<Phrase> phrases = new ArrayList<Phrase>();
+		ArrayList<Phrase> easy = new ArrayList<Phrase>();
+		ArrayList<Phrase> medium = new ArrayList<Phrase>();
+		ArrayList<Phrase> hard = new ArrayList<Phrase>();
+		
 
 		try {
 			Scanner reader = new Scanner(new File(inputFile));
 			while (reader.hasNext()) {
-				phrases.add(new Phrase(reader.nextLine()));
+				Phrase phrase = new Phrase(reader.nextLine());
+				if(phrase.getNumWords() == 5)
+				{
+					easy.add(phrase);
+				}
+				else if(phrase.getNumWords() == 7)
+				{
+					medium.add(phrase);
+				}
+				else if(phrase.getNumWords() == 10)
+				{
+					hard.add(phrase);
+				}
 			}
-
 			reader.close();
 		} catch (Exception e) {
 			System.out.println("Error located " + e.getMessage());
 		}
-
-		//TODO Temporary to test phrases
-		for (int i = 0; i < phrases.size(); i++) {
-			System.out.println(phrases.get(i).toString());
-		}
-		
-		System.out.println(phrases.get(0).compareTo(phrases.get(0))); //TODO Temporary - test the compareTo method
 		
 		new Frame(); // This brings up the window
+		
 	}
 	
 }
